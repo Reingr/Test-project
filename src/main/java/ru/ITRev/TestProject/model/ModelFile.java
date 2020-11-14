6989af430aa82c6b62aef174d12324a8de09c0ce
reinger <denis.bolotov.98@mail.ru>
2020-11-14 17:20:30 +0500

    Доработал сравнение файлов

--- src/main/java/ru/ITRev/TestProject/model/ModelFile.java
+++ src/main/java/ru/ITRev/TestProject/model/ModelFile.java
@@ -22,19 +22,18 @@ public class ModelFile {
     private byte[] file;
 
     public boolean equals(MultipartFile file) throws IOException {
-        //ToDo добавить в условие имя и тип файла
-        if (Arrays.equals(getFile(), file.getBytes()) && file.getSize()!=getSize()){
-            return true;
-        }
-        return false;
+        String fileName = file.getOriginalFilename();
+        return Arrays.equals(getFile(), file.getBytes()) && file.getSize() == getSize()
+                && FilenameUtils.getBaseName(fileName).equals(getName())
+                && FilenameUtils.getExtension(fileName).equals(getFormatFile().getName());
     }
 
-    public void setNameAndFormat(String fileName){
+    public void setNameAndFormat(String fileName) {
         setName(FilenameUtils.getBaseName(fileName));
         setFormatFile(FormatFile.fromString(FilenameUtils.getExtension(fileName)));
     }
 
-    public String getOriginalFilename(){
-        return getName()+"."+getFormatFile().getName();
+    public String getOriginalFilename() {
+        return getName() + "." + getFormatFile().getName();
     }
 }
